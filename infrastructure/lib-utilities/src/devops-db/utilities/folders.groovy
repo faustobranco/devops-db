111a4744ef5bdd703e8602ad4df1c95a8528c174
Fausto Fiorese Branco <fausto.branco@gmail.com>
2024-05-09 23:21:40 +0100

    [FEAT] shared library

---
+++ infrastructure/lib-utilities/src/devops-db/utilities/folders.groovy
@@ -0,0 +1,9 @@
+def CreateFolders(String str_structure){
+  if (str_structure.isEmpty()) {
+    throw new Exception("str_structure parameter is Empty");
+  }
+  File directory = new File(str_structure);
+  if (! directory.exists()){
+    directory.mkdirs();
+  }
+}
